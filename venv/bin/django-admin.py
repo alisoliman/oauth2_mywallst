082393f3a30248ef04df68ce563e8d061d40bade
Ali Soliman <ali.soliman95@gmail.com>
2019-09-12 01:15:20 +0200

    Remove venv

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/alisoliman/Development/Python/Django/oauth2_mywallst/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
